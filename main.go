f01000edb847fe86ada2488bd8bd8dd8da1b69ce
Ben Lubar <ben.lubar@gmail.com>
2013-06-04 11:16:09 -0500

    Starting a new project: "Forum"
    Forum will be easy to use, secure, and fast. I'm also planning on adding
    scriptable components that will use admin-configurable JavaScript when
    certain actions are being performed ("run this to see if the user can
    actually post").
    
    This initial commit contains a stub of a README, enough code to connect
    to a Couchbase server and securely display a 404 page, profiling support
    (go build -tags profile), and a 1024-bit TLS key valid until the end of
    the year that can be used for local testing (it's valid on 127.0.0.1 and
    localhost. You can generate your own using the script in Go's crypto/tls
    package).

---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"crypto/tls"
+	"flag"
+	"log"
+	"net"
+	"net/http"
+
+	"github.com/couchbaselabs/go-couchbase"
+)
+
+var Bucket *couchbase.Bucket
+
+func main() {
+	var (
+		addr = flag.String("addr", ":8015", "address to listen on")
+		cert = flag.String("tls-cert", "", "cert.pem file")
+		key  = flag.String("tls-key", "", "key.pem file")
+
+		cburl    = flag.String("couchbase", "http://127.0.0.1:8091", "")
+		cbpool   = flag.String("cb-pool", "default", "")
+		cbbucket = flag.String("cb-bucket", "default", "")
+	)
+
+	flag.Parse()
+
+	if (*cert == "") != (*key == "") {
+		log.Fatalf("the -tls-cert flag requires the -tls-key flag, and vice-versa.")
+	}
+
+	b, err := couchbase.GetBucket(*cburl, *cbpool, *cbbucket)
+	if err != nil {
+		log.Fatalf("error connecting to Couchbase: %v", err)
+	}
+	Bucket = b
+
+	ln, err := net.Listen("tcp", *addr)
+	if err != nil {
+		log.Fatalf("error listening on %q: %v", *addr, err)
+	}
+
+	if *cert != "" {
+		config := &tls.Config{
+			NextProtos:   []string{"http/1.1"},
+			Certificates: make([]tls.Certificate, 1),
+		}
+		config.Certificates[0], err = tls.LoadX509KeyPair(*cert, *key)
+		if err != nil {
+			log.Fatalf("error loading TLS data: %v", err)
+		}
+		ln = tls.NewListener(ln, config)
+	}
+
+	log.Printf("Now listening on %v", ln.Addr())
+	err = http.Serve(ln, nil)
+	if err != nil {
+		log.Fatalf("fatal error: %v", err)
+	}
+}
---
+++ profile.go
@@ -0,0 +1,5 @@
+// +build profile
+
+package main
+
+import _ "net/http/pprof"
